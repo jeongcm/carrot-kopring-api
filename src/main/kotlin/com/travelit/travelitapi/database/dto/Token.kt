b813a672f4c0cae9aeeaa1a935e7e75288feede2
jeongcm <jeongcm0101@gmail.com>
2023-11-03 13:33:33 +0900

    tvl-47 / token entity 해제

--- src/main/kotlin/com/travelit/travelitapi/database/dto/Token.kt
+++ src/main/kotlin/com/travelit/travelitapi/database/dto/Token.kt
@@ -1,19 +1,11 @@
 package com.travelit.travelitapi.database.dto
 
-import jakarta.persistence.*
 import lombok.AllArgsConstructor
 import lombok.NoArgsConstructor
 
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 data class Token (
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
-
-        @Column(nullable = true)
         val accessToken: String? = null,
-        @Column(nullable = false)
         val refreshToken: String? = null
 )
