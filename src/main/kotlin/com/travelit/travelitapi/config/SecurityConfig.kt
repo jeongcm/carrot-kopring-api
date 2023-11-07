b26ad6d9914ed40ceb3af78df891aaeb8081b21e
jeongcm <jeongcm0101@gmail.com>
2023-11-07 16:47:17 +0900

    tvl-47 / SecurityConfig 작성

---
+++ src/main/kotlin/com/travelit/travelitapi/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.travelit.travelitapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity) = http
+        .csrf{
+            it.disable()  // csrf 설정하지 않음
+        }
+        .authorizeHttpRequests {
+            it.requestMatchers("/","/auth/logIn", "/auth/signUp").permitAll() // 로그인 회원가입 관련 router 와 / 는 인증 제외
+                .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+        }
+        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) } // 세션을 사용하지 않음
+        .build()!! // !! 은 null이 아님을 보장
+}
