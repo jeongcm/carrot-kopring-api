627344b16ccc4cb1d4e995398758a4e636fdc937
jeongcm <jeongcm0101@gmail.com>
2024-01-15 17:00:40 +0900

    feature/queryDsl queryDsl 테스트용 account service 추가 개발

--- src/main/kotlin/com/carrot/kopring/account/repository/AccountRepository.kt
+++ src/main/kotlin/com/carrot/kopring/account/repository/AccountRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountRepository : JpaRepository<Account, Long> {
+interface AccountRepository : JpaRepository<Account, Long>, AccountRepositoryCustom {
     fun findByName(name: String): Account?
     fun findAllByName(name: String?): List<Account>?
     fun findByEmail(email: String?): Account?
