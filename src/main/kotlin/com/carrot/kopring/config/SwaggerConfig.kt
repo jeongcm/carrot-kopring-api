0077e596282f354ff3318d6eb57512c1e34004cc
jeongcm <jeongcm0101@gmail.com>
2024-01-18 16:42:12 +0900

    feature/Swagger SwaggerConfig 추가

--- src/main/kotlin/com/carrot/kopring/config/SwaggerConfig.kt
+++ src/main/kotlin/com/carrot/kopring/config/SwaggerConfig.kt
@@ -1,9 +1,30 @@
 package com.carrot.kopring.config
 
 
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
+    // SecuritySecheme명
+    private val jwtSchemeName = "Bearer Authentication";
+
+    val component: Components = Components().addSecuritySchemes(jwtSchemeName, createAPIKeyScheme())
+    val securityRequirement: SecurityRequirement = SecurityRequirement().addList(jwtSchemeName)
+    @Bean
+    fun openAPI() : OpenAPI = OpenAPI().components(component).info(apiInfo()).addSecurityItem(securityRequirement)
+
+    private fun apiInfo() = io.swagger.v3.oas.models.info.Info()
+        .title("Travelit-Backend-API")
+        .description("travelit backend swagger ui")
+        .version("1.0.0")
+
+    private fun createAPIKeyScheme() : SecurityScheme = SecurityScheme().type(SecurityScheme.Type.HTTP)
+        .bearerFormat("JWT")
+        .scheme("bearer")
 
 }
