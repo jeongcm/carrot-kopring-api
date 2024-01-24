e81cb21faed127416344d91d3050020299391ea2
jeongcm <jeongcm0101@gmail.com>
2024-01-24 17:38:57 +0900

    feature/feed 기본 feed controller 제작

--- src/main/kotlin/com/carrot/kopring/feed/controller/FeedController.kt
+++ src/main/kotlin/com/carrot/kopring/feed/controller/FeedController.kt
@@ -2,14 +2,31 @@ package com.carrot.kopring.account.controller
 
 import com.carrot.kopring.feed.service.FeedService
 import com.carrot.kopring.common.logger.logger
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.carrot.kopring.common.security.response.ResponseDto
+import com.carrot.kopring.database.entity.Feed
+import com.carrot.kopring.feed.dto.FeedDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/feed")
+@RequestMapping("/api/v1")
 class FeedController(var feedService: FeedService) {
     var logger = logger()
 
+    @PostMapping("/feed")
+    fun postFeed() : ResponseEntity<FeedDto> {
+        return ResponseEntity.ok().body(null)
+    }
+    @GetMapping("/feed")
+    fun getFeedList() : ResponseEntity<List<FeedDto>> {
+        return ResponseEntity.ok().body(listOf())
+    }
+
+    @GetMapping("/feed/{feedId}")
+    fun getFeed(@PathVariable feedId: Int) : ResponseEntity<List<FeedDto>> {
+
+        return ResponseEntity.ok().body(listOf())
+    }
     // feed list
 
     // make feed
